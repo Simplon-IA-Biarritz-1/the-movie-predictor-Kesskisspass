19eb8bff8fe1a298e434bc426f37abe7736a8224
kesskisspass <kesskisspass@gmail.com>
2020-03-23 23:03:51 +0100

    typo

--- data_transform.py
+++ data_transform.py
@@ -7,13 +7,13 @@ class data_transform:
     def __init__(self):
         pass
     
-    def spliter(self, data, key):
+    def splitter_cell(self, data, key):
 
         data[key] = data[key].split(",")
         return data
         
     
-    def to_integer(self, data, key):
+    def to_int(self, data, key):
 
         if data[key] != "\\N":
             data[key] = int(data[key])
