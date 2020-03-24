6279ae0556292321740342be3af232e1259614ee
kesskisspass <kesskisspass@gmail.com>
2020-03-24 11:18:49 +0100

    name basics  class ok

--- datatype.py
+++ datatype.py
@@ -1,12 +1,14 @@
 class NAME_BASICS():
     def __init__(self, dico):
-        self.birthYear = dico['birthYear'].to_int()
+        self._id = dico['nconst']
+        self.nconst = dico['nconst']
+        self.primaryName = dico['primaryName']
+        self.birthYear = None if dico['birthYear'] == "\\N" else int(
+            dico['birthYear'])
+        self.deathYear = None if dico['deathYear'] == "\\N" else int(
+            dico['deathYear'])
+        self.primaryProfession = dico['primaryProfession'].split(",")
+        self.knownForTitles = dico['knownForTitles'].split(",")
 
-    def to_int(self):
-        if self != "\\N":
-            self = int(self)
-        else:
-            self = None
-        return self
 
 
